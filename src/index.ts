170f1e2dc484d9ab17afa42848846ed9aaba5733
Joe-Mewoabi <joe@kejangnoah.com>
2023-11-23 20:14:10 +0100

    multiply two numbers

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ const addTwoNumbers = (a: number, b: number) => {
 	return a + b;
 };
 
+const multiplyTwoNumbers = (a: number, b: number) => {
+	return a * b;
+};
+
 const result = addTwoNumbers(2, 4);
 
+const result2 = addTwoNumbers(2, 4);
+
 console.log("result", result);
+console.log("result2", result2);
